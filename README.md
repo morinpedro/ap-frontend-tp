37610b2c274fd6d96fafd88f4a87aea317e00065
Pedro Morin <morinpedro@gmail.com>
2023-01-26 12:30:29 -0300

    commit inicial

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # ap-frontend-tp
+
+Este es el trabajo práctico del curso
+Primeros pasos en el desarrollo frontend
+de Front end Academy - 2022
+Argentina Programa 4.0
+
+Curriculum en html con javascript.
---
+++ js/script.js
@@ -0,0 +1,21 @@
+
+for(let i = 0; i< 7; i++) {
+  document.getElementById('botonMostrarResumen'+i).addEventListener('click', function() {
+    mostrarResumen(i);
+  })
+  document.getElementById('botonOcultarResumen'+i).addEventListener('click', function() {
+    ocultarResumen(i);
+  })
+}
+
+function mostrarResumen(numero) {
+  document.getElementById('resumen'+numero).style.display="block";
+  document.getElementById('botonMostrarResumen'+numero).style.display="none";
+  document.getElementById('botonOcultarResumen'+numero).style.display="inline";
+}
+
+function ocultarResumen(numero) {
+  document.getElementById('resumen'+numero).style.display="none";
+  document.getElementById('botonMostrarResumen'+numero).style.display="inline";
+  document.getElementById('botonOcultarResumen'+numero).style.display="none";
+}
